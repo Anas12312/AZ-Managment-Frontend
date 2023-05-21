6a4ebc0104d42843736fcab75e875ea3b8b866db
Zyad Abdul-Nasser <zizo_007@live.com>
2023-05-22 01:42:03 +0300

    'zyad'

--- src/pages/Signup/SignupForm/SignupForm.jsx
+++ src/pages/Signup/SignupForm/SignupForm.jsx
@@ -1,16 +1,44 @@
 import React, { useState } from 'react'
+import Button from '../../../components/Button'
+import config from '../../../../config'
+import { useNavigate } from 'react-router-dom';
 
 export default function SignupForm() {
+
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-    const signup = (e) => {
-        console.log(email);
+    const nav = useNavigate();
+
+    const signup = () => {
+        fetch(`${config.BASE_URL}/account/signup`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: username,
+                email,
+                password
+            })
+        }).then(res => res.json())
+        .then(respone => {
+            if(respone.error) {
+                setError(respone.error);
+            }
+            else {
+                localStorage.setItem('token', respone.token);
+                nav('/home', { replace: true });
+            }
+        })
     }
 
   return (
     <>
+        <p>{error}</p>
+
         <p>Username</p>
         <input type='text' name='username' value={username} onChange={(e) => {
             setUsername(e.target.value);
@@ -27,7 +55,7 @@ export default function SignupForm() {
         }}/>
 
         <div>
-            <button onClick={signup} >Sign Up</button>
+            <Button title='SignUp' onClick={signup} />
         </div>
     </>
   )
