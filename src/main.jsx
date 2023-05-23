860cb3edb3604e0d3e00bd97c343673c398f3d54
Zyad Abdul-Nasser <zizo_007@live.com>
2023-05-23 17:20:00 +0300

    'zyad'

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,7 @@ import './Styles/index.css'
 import AppRouter from './router/AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <div>
-        <div className='text-3xl font-bold underline'>
-            aa
-        </div>
+    <div className=' bg-gray-800 container mx-auto px-6 text-neutral-200'>
         <AppRouter />
     </div>
 )
