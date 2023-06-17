7b5c9387783adb0ce51ee6cbbaa624ab5604410f
anas12312 <anashesham000@gmail.com>
2023-06-17 22:31:10 +0300

    anos

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -24,7 +24,7 @@ export default function Login() {
         </div> */}
           <div className='mb-5 text-2xl'><span>Login to AZ</span></div>
           <LoginForm />
-          <div className='flex mt-4 border w-80 h-16 justify-center items-center rounded-md bg-accent'>
+          <div className='flex mt-4 border w-80 h-16 justify-center items-center rounded-md bg-opacity-5 bg-white'>
             <span className='text-sm'>Don't Have an Account? <Link text="Signup" onClick={()=>nav("/signup")}/></span>
           </div>
         </div>
