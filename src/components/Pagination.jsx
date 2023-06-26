fcffc898b5b18c4c4fd7ce23875a78f9a92f3891
anas12312 <anashesham000@gmail.com>
2023-06-26 16:38:08 +0300

    'anas'

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -14,16 +14,16 @@ export default function ({count, page, setPage}) {
         <nav aria-label="Page navigation example" className='flex justify-center'>
             <ul className="inline-flex -space-x-px">
                 <li>
-                    <a href="#" className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                    <a href="#" className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">"Previous</a>
                 </li>
                 {
                     Array.from(Array(parseInt(count/6)+1).keys()).map((ele, no)=> (
                         <li key={no+1} onClick={() => {setPage(no)}}>
-                            <a href="#" id={no} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{no+1}</a>
+                            <a href="#" id={no} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">{no+1}</a>
                         </li>))
                 }
                 <li>
-                    <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                    <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 ">Next</a>
                 </li>
             </ul>
         </nav>
