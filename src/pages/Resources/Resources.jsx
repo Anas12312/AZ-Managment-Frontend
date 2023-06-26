8db087c859e25f30d5ab0cae95d9ef6af8dec7f2
anas12312 <anashesham000@gmail.com>
2023-06-26 14:15:09 +0300

    anas

--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -3,13 +3,14 @@ import NavBar from '../../components/NavBar/NavBar'
 import SideBar from '../../components/SideBar/SideBar'
 import Card from '../../components/Card'
 import config from '../../../config';
+import Pagination from '../../components/Pagination';
 
 export default function Resources() {
   
   const [units, setUnits] = useState([]);
 
   const getAllUserUnits = async () => {
-    const res = await fetch(config.BASE_URL + "/units?page=0&limit=10", {
+    const res = await fetch(config.BASE_URL + "/units?page=0&limit=2", {
       method: "GET",
       headers:  
       { 
@@ -30,10 +31,15 @@ export default function Resources() {
         <NavBar selected={"files"}/>
         <div className='flex flex-row h-screen'>  
             <SideBar />
-            <div className='w-full overflow-auto max-h-screen pt-4 pb-10'>
-              {units.map((unit, i) => (
-                <Card key={i} {...unit}/>
-              ))}
+            <div className='w-full'>
+              <div className='w-full overflow-auto max-h-screen pt-4 pb-10'>
+                {units.map((unit, i) => (
+                  <Card key={i} {...unit}/>
+                ))}
+              </div>
+              <div className=''>
+                <Pagination />
+              </div>
             </div>
         </div>
     </div>
