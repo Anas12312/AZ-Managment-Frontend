ef8a0434790203741ee52f7217e85de6bc867721
anas12312 <anashesham000@gmail.com>
2023-07-03 20:03:38 +0300

    'anas'

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,11 +1,53 @@
 import React, { useState } from 'react'
 import { FaArrowDown, FaArrowUp, FaLink, FaImage, FaFile, FaPencilAlt, FaTrash, FaEye, FaPlus, FaFileAlt } from 'react-icons/fa'
+import Modal from 'react-modal';
 import config from '../config';
 export default function Item(props) {
   const [ItemData, setItemData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [expand, setExpand] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modal2IsOpen, setModal2IsOpen] = useState(false);
+  const [deletingId, setDeletingId] = useState(null);
+  const [deleteResponse, setDeleteResponse] = useState('');
+  const [deleteStatus, setDeleteStatus] = useState(200);
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
+  const closeModal = () => {
+    setModalIsOpen(false);
+  }
+  const openModal2 = () => {
+    setModal2IsOpen(true);
+  }
+  const closeModal2 = () => {
+    setModal2IsOpen(false);
+    getItemData()
+  }
+  const deleteResource = async (deletingId) => {
+    fetch(config.BASE_URL + `/resource/${deletingId}`, {
+      method: "DELETE",
+      headers:  
+      { 
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem('token')
+      }
+    }).then((res) => {
+      setDeleteStatus(res.status)
+      return res.json()
+    })
+    .then((response)=>{
+      setDeleteResponse(response)
+      closeModal();
+      openModal2();
+    }).catch((err)=>{
+      setDeleteResponse(err)
+      closeModal();
+      openModal2();
+    }); 
+  }
   let circleCommonClasses = 'h-2 w-2 bg-primary-1   rounded-full';
+  const rootEl = document.getElementById('root');
   const getItemData = async() => {
     fetch(config.BASE_URL + `/nodes/${props._id}`, {
       method: "GET",
@@ -26,6 +68,45 @@ export default function Item(props) {
   
   return (
     <div>
+      <Modal
+        appElement={rootEl}
+        isOpen={modalIsOpen}
+        className={'h-1/4 w-1/4 left-1/3 top-1/3 absolute flex flex-col justify-between items-center border p-5 rounded-md bg-secondary-2 text-black'}
+        shouldFocusAfterRender={false}
+      >
+        <div className='font-bold mt-5'>
+          Are you sure you want to delete this?
+        </div>
+        <div className='flex justify-between w-[50%] mb-7 items-center'>
+          <div className='bg-primary-1 text-white p-2 hover:cursor-pointer hover:bg-primary-2 rounded-md shadow-lg'
+              onClick={()=>{deleteResource(deletingId)}}>Delete</div>
+          <div className='bg-white text-black p-2 hover:cursor-pointer hover:bg-gray-300 rounded-md'
+                onClick={()=>{closeModal()}}>Cancel</div>
+        </div>
+      </Modal>
+      <Modal
+        appElement={rootEl}
+        isOpen={modal2IsOpen}
+        className={'h-1/6 w-1/6 left-[40%] top-1/3 absolute flex flex-col justify-between items-center border rounded-md bg-secondary-2 text-black'}
+        shouldFocusAfterRender={false}
+        onRequestClose={closeModal2}
+      >
+        {deleteStatus == 200?(
+          <div className='w-full h-full bg-green-200 border border-green-400 flex justify-center items-center bg-opacity-50'>
+            Deleted Successfully!
+          </div>
+        ):(
+          deleteResponse?(
+            <div className='w-full h-full bg-red-200 border border-red-400 flex justify-center items-center bg-opacity-50'>
+              Resource Not Found!
+            </div>
+          ):(
+            <div className='w-full h-full bg-red-200 border border-red-400 flex justify-center items-center bg-opacity-50'>
+              An Error Occured Try Again Later
+            </div>
+          )
+        )}
+      </Modal>
       <div className='node-container'>
         <div className='item' onClick={() => {
             getItemData();
@@ -49,7 +130,7 @@ export default function Item(props) {
                         <div className='mx-3'><FaLink /></div>
                         <div className='w-[50%] mr-2' >{resource.name}</div>
                         <div className='flex items-center justify-start w-[30%] relative'>
-                          {resource.createdByImageUrl?(
+                          {resource.createdBy.imgUrl?(
                             <div className='w-7 h-7 flex justify-center items-center
                                           bg-primary-1 rounded-full text-xl
                                           text-white flex-shrink-0 flex-grow-0'>
@@ -59,7 +140,7 @@ export default function Item(props) {
                             <div className='w-full h-full flex justify-center
                                             items-center bg-primary-1 rounded-full
                                             text-xl text-white'>
-                              {resource.createdByName[0].toUpperCase()}
+                              {resource.createdBy.name[0].toUpperCase()}
                             </div>
                           )}
                           <div className='mx-2 text-sm font-bold'>
@@ -69,7 +150,10 @@ export default function Item(props) {
                         <div className='flex relative -right-[4.5rem]'>
                           <div className='resource-option'><FaEye /></div>
                           <div className='resource-option'><FaPencilAlt /></div>
-                          <div className='resource-option'><FaTrash /></div>
+                          <div className='resource-option' onClick={()=>{
+                            setDeletingId(resource._id)
+                            openModal()
+                          }}><FaTrash /></div>
                         </div>
                       </div>
                     )
@@ -79,7 +163,7 @@ export default function Item(props) {
                         <div className='mx-3'><FaFileAlt /></div>
                         <div className='w-[50%] mr-2'>{resource.name}</div>
                         <div className='flex items-center justify-start w-[30%] relative'>
-                          {resource.createdByImageUrl?(
+                          {resource.createdBy.imgUrl?(
                             <div className='w-7 h-7 flex justify-center items-center
                                           bg-primary-1 rounded-full text-xl
                                           text-white flex-shrink-0 flex-grow-0'>
@@ -90,7 +174,7 @@ export default function Item(props) {
                                             items-center bg-primary-1 rounded-full
                                             text-xl 
                                             text-white'>
-                              {resource.createdByName[0].toUpperCase()}
+                              {resource.createdBy.name[0].toUpperCase()}
                             </div>
                           )}
                           <div className='mx-2 text-sm font-bold'>
@@ -100,7 +184,10 @@ export default function Item(props) {
                         <div className='flex relative -right-[4.5rem]'>
                           <div className='resource-option'><FaEye /></div>
                           <div className='resource-option'><FaPencilAlt /></div>
-                          <div className='resource-option'><FaTrash /></div>
+                          <div className='resource-option' onClick={()=>{
+                            setDeletingId(resource._id)
+                            openModal()
+                          }}><FaTrash /></div>
                         </div>
                       </div>
                     )
@@ -121,7 +208,7 @@ export default function Item(props) {
                                             items-center bg-primary-1 rounded-full
                                             text-xl 
                                             text-white'>
-                              {resource.createdByName[0].toUpperCase()}
+                              {resource.createdBy.name[0].toUpperCase()}
                             </div>
                           )}
                           <div className='mx-2 text-sm font-bold'>
@@ -131,7 +218,10 @@ export default function Item(props) {
                         <div className='flex relative -right-[4.5rem]'>
                           <div className='resource-option'><FaEye /></div>
                           <div className='resource-option'><FaPencilAlt /></div>
-                          <div className='resource-option'><FaTrash /></div>
+                          <div className='resource-option' onClick={()=>{
+                            setDeletingId(resource._id)
+                            openModal()
+                          }}><FaTrash /></div>
                         </div>
                       </div>
                     )
