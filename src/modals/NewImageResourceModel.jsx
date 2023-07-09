4385fce9e79358005338a3b83423598d8ede9ae3
Zyad Abdul-Nasser <zizo_007@live.com>
2023-07-10 00:06:37 +0300

    'zyad'

--- src/modals/NewImageResourceModel.jsx
+++ src/modals/NewImageResourceModel.jsx
@@ -4,83 +4,94 @@ import config from '../../config'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form';
 
-export default function NewImageResourceModel({isOpen, setIsOpen, nodeId, getItemData}) {
+export default function NewImageResourceModel({ isOpen, setIsOpen, nodeId, getItemData }) {
 
-    const [name, setName] = useState('')
-    const [error, setError] = useState('')
+  const [name, setName] = useState('')
+  const [error, setError] = useState('')
 
-    const closeNewResModal = () => {
+  const closeNewResModal = () => {
+    setName('')
+    setError('')
+    setSelectedFile(null)
+    setIsFilePicked(false)
+    setIsOpen(false);
+  }
+
+  const finalCloseNewResModal = () => {
+    setTimeout(() => {
       setName('')
       setError('')
       setSelectedFile(null)
-      setIsFilePicked(false)  
+      setIsFilePicked(false)
       setIsOpen(false);
+    }, 1000)
+  }
+
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const changeHandler = (event) => {
+    
+    if (event.target.files[0].type === 'image/jpeg' || event.target.files[0].type === 'image/png' || event.target.files[0].type === 'image/jpg') {
+      setSelectedFile(event.target.files[0]);
+      setIsFilePicked(true);
+      const output = document.getElementById('preview')
+      output.src = URL.createObjectURL(event.target.files[0]);
+    } else {
+      setError("Image Type must be JPG, JPEG or PNG")
     }
+  };
 
-    const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
+  const handleSubmission = () => {
+    const formData = new FormData();
 
-    const changeHandler = (event) => {
-      console.log(event.target.files[0])
-      if(event.target.files[0].type === 'image/jpeg' || event.target.files[0].type === 'image/png' || event.target.files[0].type === 'image/jpg') {
-        setSelectedFile(event.target.files[0]);
-        setIsFilePicked(true);
-        const output = document.getElementById('preview')
-        output.src = URL.createObjectURL(event.target.files[0]);
-      }else {
-        setError("Image Type must be JPG, JPEG or PNG")
+    formData.append('file', selectedFile);
+
+    fetch(
+      config.BASE_URL + '/upload',
+      {
+        method: 'POST',
+        body: formData,
       }
-    };
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        addResource(nodeId, result.message);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
-    const handleSubmission = () => {
-      const formData = new FormData();
-  
-      formData.append('file', selectedFile);
-  
-        fetch(
-          config.BASE_URL + '/upload',
-          {
-            method: 'POST',
-            body: formData,
+  const addResource = (nodeId, imageUrl) => {
+    finalCloseNewResModal()
+    
+    fetch(config.BASE_URL + '/resource/' + nodeId, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem('token')
+      },
+      body: JSON.stringify(
+        {
+          name: name,
+          type: 'IMAGE',
+          data: {
+            imageUrl: imageUrl
           }
-        )
-        .then((response) => response.json())
-        .then((result) => {
-          addResource(nodeId, result.message);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-    };
-
-    const addResource = (nodeId, imageUrl) => {
-        fetch(config.BASE_URL + '/resource/' + nodeId , {
-            method:"POST",
-            headers:  { 
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + localStorage.getItem('token')
-            },
-            body: JSON.stringify(
-                {
-                  name: name,
-                  type: 'IMAGE',
-                  data: {
-                    imageUrl: imageUrl
-                  }
-                }
-            )
-        }).then(res => res.json())
-        .then(result => {
-          getItemData() 
-          closeNewResModal()
-        }).catch((err) => {
-          console.log(err);
-        })
-    }
+        }
+      )
+    }).then(res => res.json())
+      .then(result => {
+        getItemData()
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
 
   return (
     <div>
-        <Modal
+      <Modal
         appElement={document.getElementById('root')}
         isOpen={isOpen}
         className={'h-fit w-1/3 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 absolute flex flex-col justify-between items-center border p-5 rounded-md bg-secondary-2 text-black'}
@@ -90,39 +101,39 @@ export default function NewImageResourceModel({isOpen, setIsOpen, nodeId, getIte
 
         <div><span className='text-sm ml-1 text-red-600 font-bold'>{error}</span></div>
         <div className='w-full'>
-            <div><label htmlFor="Res-Name" id='Res-Name-Lable' className='text-sm ml-1'>Name</label></div>
-            <input  
+          <div><label htmlFor="Res-Name" id='Res-Name-Lable' className='text-sm ml-1'>Name</label></div>
+          <input
             id='Res-Name'
-             className='text-sm w-full my-1 h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
-             type='text'
-             onChange={(e) => {
-                setName(e.target.value)
-             }}
-            />
+            className='text-sm w-full my-1 h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
+            type='text'
+            onChange={(e) => {
+              setName(e.target.value)
+            }}
+          />
         </div>
 
         <div className='w-full'>
-            <div><label htmlFor="Res-Text" className='text-sm ml-1'>Image</label></div>
-            <input  id='Res-Text'  className='resize-none text-sm w-full max-h-md my-1 h-16 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
+          <div><label htmlFor="Res-Text" className='text-sm ml-1'>Image</label></div>
+          <input id='Res-Text' className='resize-none text-sm w-full max-h-md my-1 h-16 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
             type='file'
             accept='.jpg, .jpeg, .png'
-            onChange={changeHandler}/>
-            <div className='w-full flex justify-center bg-secondary-3 rounded-md border border-primary-1 p-2 mb-1'>
-              <img className='w-96 border-2 rounded-md max-h-80 ' id='preview' src='' />
-            </div>
+            onChange={changeHandler} />
+          <div className='w-full flex justify-center bg-secondary-3 rounded-md border border-primary-1 p-2 mb-1'>
+            <img className='w-96 border-2 rounded-md max-h-80 ' id='preview' src='' />
+          </div>
         </div>
 
         <div className='w-full'>
-            <button className="w-full  h-9 rounded-md border text-sm bg-primary-2 text-white hover:bg-primary-1"
-              onClick={() => {
-                if(name) {
-                    handleSubmission();
-                }
-                else {
-                    setError('Please provide name or image for the resource')
-                }
-              }}
-            >Create Resource</button>
+          <button className="w-full  h-9 rounded-md border text-sm bg-primary-2 text-white hover:bg-primary-1"
+            onClick={() => {
+              if (name) {
+                handleSubmission();
+              }
+              else {
+                setError('Please provide name or image for the resource')
+              }
+            }}
+          >Create Resource</button>
         </div>
 
       </Modal>
--- src/modals/NewLinkResourceModal.jsx
+++ src/modals/NewLinkResourceModal.jsx
@@ -3,7 +3,7 @@ import Modal from 'react-modal'
 import config from '../../config'
 import { LoadNodesContext } from '../pages/ViewUnit/ViewUnit'
 
-export default function LinkResourceModal({isOpen, isEdit, setIsOpen, nodeId, getItemData, _id, link:oldlink, name:oldname}) {
+export default function LinkResourceModal({ isOpen, isEdit, setIsOpen, nodeId, getItemData, _id, link: oldlink, name: oldname }) {
 
     const [name, setName] = useState('')
     const [embedName, setEmbedName] = useState('')
@@ -18,170 +18,182 @@ export default function LinkResourceModal({isOpen, isEdit, setIsOpen, nodeId, ge
         setLink(oldlink)
     }, [oldname])
 
+    const finalCloseNewResModal = () => {
+        setTimeout(() => {
+            setEmbedName('')
+            setIsOpen(false);
+        }, 1000)
+    }
+
     const closeNewResModal = () => {
-        setEmbedName('')
-        setIsOpen(false);
+        setTimeout(() => {
+            setEmbedName('')
+            setIsOpen(false);
+        }, 1000)
     }
 
     const getEmbedName = (link) => {
-        fetch(config.BASE_URL + '/oembed' , {
-            method:"POST",
-            headers:  { 
+        fetch(config.BASE_URL + '/oembed', {
+            method: "POST",
+            headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + localStorage.getItem('token')
             },
             body: JSON.stringify(
                 {
-                  url: link
+                    url: link
                 }
             )
         }).then(res => res.json())
-        .then(result => {
-            if(result.status !== 404) {
-                setEmbedName(result.title)
-                console.log(result.title);
-            }
-        }).catch((err) => {
-            console.log(err);
-        })
+            .then(result => {
+                if (result.status !== 404) {
+                    setEmbedName(result.title)
+                    console.log(result.title);
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
     }
 
     const addResource = (nodeId) => {
-        fetch(config.BASE_URL + '/resource/' + nodeId , {
-            method:"POST",
-            headers:  { 
+        finalCloseNewResModal()
+
+        fetch(config.BASE_URL + '/resource/' + nodeId, {
+            method: "POST",
+            headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + localStorage.getItem('token')
             },
             body: JSON.stringify(
                 {
-                  name: (!input1)?name:embedName,
-                  type: 'LINK',
-                  data: {
-                    link: link
-                  }
+                    name: (!input1) ? name : embedName,
+                    type: 'LINK',
+                    data: {
+                        link: link
+                    }
                 }
             )
         }).then(res => res.json())
-        .then(result => {
-            closeNewResModal()
-            getItemData()
-        }).catch(err => {
-            console.log(err);
-        })
+            .then(result => {
+
+                getItemData()
+            }).catch(err => {
+                console.log(err);
+            })
     }
 
     const editResource = (resId) => {
-        fetch(config.BASE_URL + '/resource/' + resId , {
-            method:"PUT",
-            headers:  { 
+        finalCloseNewResModal()
+
+        fetch(config.BASE_URL + '/resource/' + resId, {
+            method: "PUT",
+            headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + localStorage.getItem('token')
             },
             body: JSON.stringify(
                 {
-                  name: (!input1)?name:embedName,
-                  type: 'LINK',
-                  data: {
-                    link: link
-                  }
+                    name: (!input1) ? name : embedName,
+                    type: 'LINK',
+                    data: {
+                        link: link
+                    }
                 }
             )
         }).then(result => {
-            closeNewResModal()
             getItemData()
         }).catch(err => {
             console.log(err);
         })
     }
 
-  return (
-    <div>
-        <Modal
-        appElement={document.getElementById('root')}
-        isOpen={isOpen}
-        className={'h-72 w-1/3 left-1/3 top-1/3 absolute flex flex-col justify-between items-center border p-5 rounded-md bg-secondary-2 text-black'}
-        shouldFocusAfterRender={false}
-        onRequestClose={closeNewResModal}
-      >
+    return (
+        <div>
+            <Modal
+                appElement={document.getElementById('root')}
+                isOpen={isOpen}
+                className={'h-72 w-1/3 left-1/3 top-1/3 absolute flex flex-col justify-between items-center border p-5 rounded-md bg-secondary-2 text-black'}
+                shouldFocusAfterRender={false}
+                onRequestClose={closeNewResModal}
+            >
 
-        <div><span className='text-sm ml-1 text-red-600 font-bold'>{error}</span></div>
-        <div className='w-full'>
-            <div><label htmlFor="Res-Link" id='Res-Link-Lable' className='text-sm ml-1'>Link</label></div>
-            <input  
-            id='Res-Link'
-             className='text-sm w-full my-1 h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
-             type='text'
-             value={link}
-             onChange={(e) => {
-                const inputLink = e.target.value
+                <div><span className='text-sm ml-1 text-red-600 font-bold'>{error}</span></div>
+                <div className='w-full'>
+                    <div><label htmlFor="Res-Link" id='Res-Link-Lable' className='text-sm ml-1'>Link</label></div>
+                    <input
+                        id='Res-Link'
+                        className='text-sm w-full my-1 h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
+                        type='text'
+                        value={link}
+                        onChange={(e) => {
+                            const inputLink = e.target.value
 
-                setLink(inputLink)
+                            setLink(inputLink)
 
-                getEmbedName(inputLink);
-             }}
-             placeholder='https://www.example.com'
-            />
+                            getEmbedName(inputLink);
+                        }}
+                        placeholder='https://www.example.com'
+                    />
+                </div>
+
+                <fieldset className='w-full mb-3'>
+                    <div><label htmlFor="Node-Name" id='Node-Name-Lable' className='text-sm ml-1'>Name</label></div>
+                    <div className='flex flex-row justify-around'>
+                        <input type='radio' name='name' className='inline mx-4'
+                            onClick={(e) => {
+                                setInput1(!input1)
+                                setInput2(!input2)
+                            }}
+
+                        />
+                        <input
+                            id='Node-Name'
+                            className='inline text-sm my-1 w-full h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
+                            type='text'
+                            disabled={input1}
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value)
+                            }}
+                            placeholder='custome name'
+                        />
+                    </div>
+                    <div className='flex flex-row justify-around'>
+                        <input type='radio' name='name' className='inline mx-4' defaultChecked={true}
+                            onClick={(e) => {
+                                setInput1(!input1)
+                                setInput2(!input2)
+                            }}
+                        />
+                        <input
+                            id='Node-Name'
+                            className='inline text-sm my-1 w-full h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
+                            type='text'
+                            disabled={input2}
+                            value={embedName}
+                            onChange={(e) => {
+                                setEmbedName(e.target.value)
+                            }}
+                            placeholder='embed name'
+                        />
+                    </div>
+                </fieldset>
+
+                <div className='w-full'>
+                    <button className="w-full  h-9 rounded-md border text-sm bg-primary-2 text-white hover:bg-primary-1"
+                        onClick={() => {
+                            if ((!input1 && name) || (!input2 && embedName)) {
+                                isEdit ? editResource(_id) : addResource(nodeId)
+                            }
+                            else {
+                                setError('Please provide name for the resource')
+                            }
+
+                        }}
+                    >{isEdit ? 'Save' : 'Create Resource'}</button>
+                </div>
+
+            </Modal>
         </div>
-
-        <fieldset className='w-full mb-3'>
-            <div><label htmlFor="Node-Name" id='Node-Name-Lable' className='text-sm ml-1'>Name</label></div>
-            <div className='flex flex-row justify-around'>
-                <input type='radio' name='name' className='inline mx-4' 
-                    onClick={(e) => {
-                        setInput1(!input1)
-                        setInput2(!input2)
-                    }}
-                    
-                />
-                <input  
-                id='Node-Name'
-                    className='inline text-sm my-1 w-full h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
-                    type='text'
-                    disabled={input1}
-                    value={name}
-                    onChange={(e) => {
-                        setName(e.target.value)
-                    }}
-                    placeholder='custome name'
-                />
-            </div>
-            <div className='flex flex-row justify-around'> 
-                <input type='radio' name='name' className='inline mx-4' defaultChecked={true}
-                    onClick={(e) => {
-                        setInput1(!input1)
-                        setInput2(!input2)
-                    }}
-                />
-                <input  
-                id='Node-Name'
-                    className='inline text-sm my-1 w-full h-8 py-1 px-2 border border-primary-1 rounded-md bg-secondary-3 '
-                    type='text'
-                    disabled={input2}
-                    value={embedName}
-                    onChange={(e) => {
-                        setEmbedName(e.target.value)
-                    }}
-                    placeholder='embed name'
-                />
-            </div>
-        </fieldset>
-
-        <div className='w-full'>
-            <button className="w-full  h-9 rounded-md border text-sm bg-primary-2 text-white hover:bg-primary-1"
-              onClick={() => {
-                if((!input1 && name) || (!input2 && embedName)) {
-                    isEdit ? editResource(_id) : addResource(nodeId) 
-                }
-                else {
-                    setError('Please provide name for the resource')
-                }
-
-              }}
-            >{isEdit? 'Save' : 'Create Resource'}</button>
-        </div>
-
-      </Modal>
-    </div>
-  )
+    )
 }
