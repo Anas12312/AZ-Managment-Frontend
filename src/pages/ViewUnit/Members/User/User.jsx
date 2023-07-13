0e123fbe4ad3a1b00dace902dfd7a372bc267891
Zyad Abdul-Nasser <zizo_007@live.com>
2023-07-13 04:11:44 +0300

    'zyad'

--- src/pages/ViewUnit/Members/User/User.jsx
+++ src/pages/ViewUnit/Members/User/User.jsx
@@ -1,21 +1,80 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
+import config from '../../../../../config'
 
-export default function User(props) {
+export default function User({ _id, name, email, username, imgUrl, status, invetationId, isOnwer, unitId }) {
     const nav = useNavigate()
-  return (
-    <div className='relative w-[95%] h-20 pl-4 flex justify-start gap-64 my-2 items-center border-2 rounded-md'>
-        <div className='absolute w-16 h-16 flex justify-center items-center rounded-full border-4 border-white'>
-            <img id='profile-pic' className='object-cover w-full h-full flex-shrink-0 rounded-full' src={props.imgUrl?props.imgUrl:"https://cdn-icons-png.flaticon.com/512/149/149071.png"} onError={() => { document.getElementById(`profile-pic`).src = "https://cdn-icons-png.flaticon.com/512/149/149071.png" }} />
+
+    const statusEl = useRef()
+
+    useEffect(() => {
+        if (status === 'OWNER') {
+            statusEl.current.classList.add('text-yellow-600')
+        }
+        if (status === 'ACCEPTED') {
+            statusEl.current.classList.add('text-green-700')
+        }
+        if (status === 'PENDING') {
+            statusEl.current.classList.add('text-slate-700')
+        }
+    })
+
+    const removeUser = () => {
+        fetch(config.BASE_URL + `/units/users/${unitId}/${invetationId}`, {
+            method:'DELETE',
+            headers:
+            {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem('token')
+            }
+        }).then(async res => {
+            if(res.status === 200) nav(0)
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    return (
+        <div className='relative w-[95%] h-20 pl-4 flex justify-start gap-64 my-2 items-center border-2 rounded-md'>
+            <div className='absolute w-16 h-16 flex justify-center items-center rounded-full border-4 border-white'>
+                <img
+                    id='profile-pic'
+                    className='object-cover w-full h-full flex-shrink-0 rounded-full'
+                    src={imgUrl ? imgUrl : "https://cdn-icons-png.flaticon.com/512/149/149071.png"}
+                    onError={() => { document.getElementById(`profile-pic`).src = "https://cdn-icons-png.flaticon.com/512/149/149071.png" }}
+                />
+            </div>
+
+            <div
+                className='absolute left-32 flex flex-col justify-around w-80 text-blue-900 hover:cursor-pointer hover:text-black'
+                onClick={() => {
+                    nav('/profile/' + username)
+                }}
+            >
+                <div>
+                    <span className='text-base font-bold'>{name}</span>
+                    <span className='text-base font-light'>({username})</span>
+                </div>
+            </div>
+
+            <div className='absolute left-[42%]'>
+                <div><span className='text-sm font-light'>{email}</span></div>
+            </div>
+
+            <div className='absolute left-[66%]'>
+                <div><span ref={statusEl} className='text-sm font-bold select-none'>{status}</span></div>
+            </div>
+
+            {isOnwer && (
+                <div className='absolute left-[90%]'>{
+                    (status !== 'OWNER') && (
+                        <div onClick={removeUser} className='cursor-pointer select-none text-white bg-red-600 hover:bg-red-800 font-medium rounded-full text-sm px-5 py-2.5 text-center'>
+                            Remove
+                        </div>
+                    )
+                }
+                </div>
+            )}
         </div>
-        <div className='absolute left-32 flex flex-col justify-around w-80 text-blue-900 hover:cursor-pointer hover:text-black' onClick={()=>{
-            nav('/profile/' + props.username)
-        }}>
-            <div><span className='text-base font-bold'>{props.name}</span> <span className='text-base font-light'>({props.username})</span></div>
-        </div>
-        <div className='absolute left-[42%]'>
-            <div><span className='text-sm font-light'>{props.email}</span></div>
-        </div>
-    </div>
-  )
+    )
 }
--- src/pages/ViewUnit/Members/ViewMembers.jsx
+++ src/pages/ViewUnit/Members/ViewMembers.jsx
@@ -5,19 +5,24 @@ import { useNavigate } from 'react-router-dom';
 import config from '../../../../config';
 import User from './User/User';
 import LoadingUser from './User/LoadingUser';
+import { FaPlus } from 'react-icons/fa';
 
 export default function ViewMembers() {
     const params = useParams()
 
     const [isLoading, setIsLoading] = useState(true);
     const [isOwner, setIsOwner] = useState(false);
-    const [unit, setUnit] = useState();
+
+    const [unit, setUnit] = useState(null)
+    const [users, setUsers] = useState([])
+
+    const [searchValue, setSearchValue] = useState('')
 
     const nav = useNavigate();
 
     const GetUnit = (id) => {
         setIsLoading(true);
-        fetch(config.BASE_URL + `/units/users/${id}`, {
+        fetch(config.BASE_URL + `/units/users/${id}?search=${searchValue}`, {
             method: "GET",
             headers:
             {
@@ -27,14 +32,18 @@ export default function ViewMembers() {
         }).then((res) => res.json())
             .then((response) => {
                 setIsLoading(false)
-                setUnit(response)
-                if (response.owner.username === JSON.parse(localStorage.getItem('user')).username) {
+                setUsers(response.users)
+                setUnit(response.unit)
+
+                if (response.unit.owner.username === JSON.parse(localStorage.getItem('user')).username) {
                     setIsOwner(true)
                 }
+
             }).catch((err) => {
                 console.log(err);
                 setUnit(null)
-                // nav(-1)
+                setUsers([])
+                nav(-1)
             });
     }
 
@@ -44,23 +53,56 @@ export default function ViewMembers() {
     return (
         <div className='flex flex-row h-[99%] top-14'>
             <UnitSideBar selected={'members'} unitId={params.id} />
-            {!isLoading?(
+            {!isLoading ? (
                 <div className='w-full'>
                     <div className='h-24 w-[98.3%] p-5 flex items-center justify-between mt-4'>
                         <div className='p-2 flex flex-col justify-center'>
                             <div className='text-2xl font-bold'>{unit.name}</div>
                             <div className='text-base font-light'>{unit.description}</div>
-                            <div className='text-base font-light'>Owner: <span onClick={() => { nav('/profile/' + unit.owner.username) }} className='text-blue-600 hover:underline hover:cursor-pointer'>{isOwner?'Me':unit.owner.name}</span></div>
+                            <div className='text-base font-light'>Owner: <span onClick={() => { nav('/profile/' + unit.owner.username) }} className='text-blue-600 hover:underline hover:cursor-pointer'>{isOwner ? 'Me' : unit.owner.name}</span></div>
                         </div>
                     </div>
 
-                    <div className='text-2xl ml-8 mt-6'>Members</div>
-                    
-                    <div className='w-full py-5 flex flex-col justify-center items-center overflow-auto'>
-                        {unit.users.map((user, index)=><User key={index} {...user} />)}
+                    <div className='text-2xl ml-8'>Members</div>
+
+                    <div className="w-full h-20 flex items-center justify-between">
+                        <div className="relative left-8 w-1/3">
+                            <form>
+                                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                                <div className="relative">
+                                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                        </svg>
+                                    </div>
+                                    <input type="search" id="default-search" value={searchValue} onChange={(e) => { setSearchValue(e.target.value) }} className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-1 focus:primary-1 " placeholder="Search Nodes or Resources" />
+                                    <button type="submit" className="text-white absolute right-2.5 bottom-2 bg-primary-1
+                                                     hover:bg-primary-2
+                                                      font-medium rounded-lg
+                                                      text-sm px-4 py-2"
+                                        onClick={(e) => {
+                                            e.preventDefault()
+                                            GetUnit(params.id)
+                                        }}>Search</button>
+                                </div>
+                            </form>
+
+                        </div>
+                        <div className="w-20 flex">
+                            <div className='w-10 h-10 hover:bg-primary-2 flex
+                                  justify-center items-center rounded-md border-2 text-white bg-primary-1 
+                                  hover:cursor-pointer transition-all transtion duration-400 ease-linear'
+                            >
+                                <FaPlus /></div>
+                        </div>
                     </div>
+
+                    <div className='w-full pb-5 flex flex-col justify-center items-center overflow-auto'>
+                        {users.map((user, index) => <User key={index} unitId={params.id} isOnwer={isOwner} {...user} />)}
+                    </div>
+
                 </div>
-            ):(
+            ) : (
                 <LoadingUser />
             )}
         </div>
