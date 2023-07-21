9c81d2e593a0c17fe2b1403f56e142b0cab52841
Zyad Abdul-Nasser <zizo_007@live.com>
2023-07-21 20:21:16 +0300

    'zyad'

--- src/components/NavBar/Notifications/CreatedNode.jsx
+++ src/components/NavBar/Notifications/CreatedNode.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function CreatedNode(props) {
+  const nav = useNavigate()
   return (
-    <div className='notification'>
+    <div className='notification'
+    onClick={(e) => { nav('/unit/'+props.unitId) }}
+    >
         <div className='relative w-11 h-11 flex justify-center items-center rounded-full border-2 border-white group my-1 mr-2'>
-            <img id='profile-pic' className='object-cover w-full h-full flex-shrink-0 rounded-full' src={props.actorId.imgUrl?props.actorId.imgUrl:"https://cdn-icons-png.flaticon.com/512/149/149071.png"} onError={() => { document.getElementById(`profile-pic`).src = "https://cdn-icons-png.flaticon.com/512/149/149071.png" }} />
+            <img id='profile-pic' 
+              className='object-cover w-full h-full flex-shrink-0 rounded-full' 
+              src={props.actorId.imgUrl?props.actorId.imgUrl:"https://cdn-icons-png.flaticon.com/512/149/149071.png"} 
+              onError={() => { document.getElementById(`profile-pic`).src = "https://cdn-icons-png.flaticon.com/512/149/149071.png" }}
+            />
         </div>
         <div className='flex flex-col justify-around w-[15rem]'>
             <div><span className='text-sm font-bold'>{props.actorId.name}</span> <span className='text-sm font-normal'>Created a Node ({props.actionId.name}) in Unit ({props.unitName})</span></div>
--- src/components/NavBar/Notifications/JoinedUnit.jsx
+++ src/components/NavBar/Notifications/JoinedUnit.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function JoinedUnit(props) {
+  const nav = useNavigate()
   return (
-    <div className='notification'>
+    <div className='notification'
+    onClick={(e) => { nav('/unit/'+props.unitId) }}>
         <div className='relative w-11 h-11 flex justify-center items-center rounded-full border-2 border-white group my-1 mr-2'>
             <img id='profile-pic' className='object-cover w-full h-full flex-shrink-0 rounded-full' src={props.actorId.imgUrl?props.actorId.imgUrl:"https://cdn-icons-png.flaticon.com/512/149/149071.png"} onError={() => { document.getElementById(`profile-pic`).src = "https://cdn-icons-png.flaticon.com/512/149/149071.png" }} />
         </div>
