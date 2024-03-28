ba2717145dbb78084cecc716190838d70a5d5fa0
anas12312 <anashesham000@gmail.com>
2024-03-29 00:28:37 +0200

    anas

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    BASE_URL : "http://localhost:3000",
+    BASE_URL : "https://az-managment-server.onrender.com",
     DEFAULT_IMAGE : './images/149071.png'
 }
 export default config;
